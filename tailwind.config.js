da960f3f14a3af77a29ee50cbf5bd88862fe7a27
Gaspar Espuña Ribas <cav12349@gmail.com>
2024-10-29 15:28:38 +0100

    same error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx, vue}",
+  ],
+    theme: {
     extend: {},
   },
   plugins: [],
 }
-
 
